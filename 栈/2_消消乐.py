b6850b96147c1dc2f9ddc7371aa77ca2cc48b85f
Mengting Niu <nmt@mail.ustc.edu.cn>
2025-08-26 16:17:33 +0800

    1

--- 栈/2_消消乐.py
+++ 栈/2_消消乐.py
@@ -1,16 +1,36 @@
+# 方法1：插入前判断
+# def elimate(arr):
+#     stack = []
+#     for i in arr:
+#         if len(stack)<=1 or stack[-1]!=i or stack[-2]!=i:
+#             stack.append(i)
+#         else:
+#             stack.pop()
+#             stack.pop()
+#     return stack
+
+# arr = list(map(int, input().split()))
+# stack = elimate(arr)
+# if not stack:
+#     print('[]')
+# else:
+#     print(' '.join(map(str, stack))) 
+
+
+# 方法2：插入后判断
 def elimate(arr):
     stack = []
     for i in arr:
-        if len(stack)<=1 or stack[-1]!=i or stack[-2]!=i:
-            stack.append(i)
-        else:
+        stack.append(i)
+        if len(stack)>=3 and stack[-1]==stack[-2]==stack[-3]==i:
+            stack.pop()
             stack.pop()
             stack.pop()
     return stack
 
 arr = list(map(int, input().split()))
-stack = elimate(arr)
-if not stack:
+result = elimate(arr)
+if not result:
     print('[]')
 else:
-    print(' '.join(map(str, stack))) 
+    print(' '.join(map(str, result)))
