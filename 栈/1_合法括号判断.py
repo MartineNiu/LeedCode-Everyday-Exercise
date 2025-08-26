d652051c894b689391e8390a3947eedf877d62c8
Mengting Niu <nmt@mail.ustc.edu.cn>
2025-08-26 12:30:39 +0800

    1

---
+++ 栈/1_合法括号判断.py
@@ -0,0 +1,13 @@
+a = [1,2,5,7,3,8]
+a.extend([1,2,3])
+a.append([19,0])
+a.reverse()
+a.insert(1,'bb')
+a.remove(1)
+b = a.pop(0)
+b = a.pop(0)
+c = sorted(a)
+a.sort(reverse = True)
+
+r = 'aonajcnonc'
+m = r.replace('a','0')
