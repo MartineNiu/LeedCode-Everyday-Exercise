62086dd89e744564b945281ec409808ee6e6a1f8
Mengting Niu <nmt@mail.ustc.edu.cn>
2025-08-27 13:54:51 +0800

    update

--- 哈希/1_塔子哥的口袋1.py
+++ 哈希/1_塔子哥的口袋1.py
@@ -1,16 +1,28 @@
+# n, q = map(int, input().split())
+# nums = list(map(int, input().split()))
+# dict = {}
+# for num in nums:
+#     if num in dict:
+#         dict[num]+=1
+#     else:
+#         dict[num]=1
+    
+# # 可以用更pythonic的风格
+# # freq = {}
+# # for num in nums:
+# #     freq[num] = freq.get(num,0)+1
+
+
+# for i in range(q):
+#     target = int(input())
+#     print(dict.get(target,0))
+
+
+from collections import Counter
 n, q = map(int, input().split())
 nums = list(map(int, input().split()))
-dict = {}
-for num in nums:
-    if num in dict:
-        dict[num]+=1
-    else:
-        dict[num]=1
-    
-for i in range(q):
-    target = int(input())
-    if target in dict:
-        print(dict[target])
-    else:
-        print(0)
+counter = Counter(nums)
 
+for _ in range(q):
+    target = int(input())
+    print(counter.get(target,0))
