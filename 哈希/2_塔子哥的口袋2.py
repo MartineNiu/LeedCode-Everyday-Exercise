746a797d0df8f975df962eeb908d08c9227a03c5
Mengting Niu <nmt@mail.ustc.edu.cn>
2025-08-27 14:56:07 +0800

    update

---
+++ 哈希/2_塔子哥的口袋2.py
@@ -0,0 +1,15 @@
+from collections import defaultdict
+
+n, q = map(int, input().split())
+nums = list(map(int, input().split()))
+positions = defaultdict(list)
+
+for idx, num in enumerate(nums):
+    positions[num].append(idx+1)
+
+for _ in range(q):
+    x, k = map(int, input().split())
+    if k>len(positions[x]):
+        print(-1)
+    else:
+        print(positions[x][k-1])
