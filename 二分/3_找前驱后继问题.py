9f874166a7a072ddb4384f49f31f69226b0db43e
Mengting Niu <nmt@mail.ustc.edu.cn>
2025-08-28 15:52:06 +0800

    update

---
+++ 二分/3_找前驱后继问题.py
@@ -0,0 +1,35 @@
+def find_left(arr, target):
+    left, right = 0, len(arr)-1
+    ans = -1
+    while left <= right:
+        mid = (left + right)//2
+        if arr[mid] < target:
+            ans = arr[mid]
+            left = mid +1
+        else:
+            right = mid-1
+    return ans
+
+def find_right(arr,target):
+    left, right = 0, len(arr)-1
+    ans = -1
+    while left <= right:
+        mid = (left+ right)//2
+        if arr[mid]>target:
+            ans = arr[mid]
+            right = mid-1
+        else:
+            left = mid+1
+    return ans
+
+def main():
+    n, q = map(int, input().split())
+    arr = list(map(int, input().split()))
+    for _ in range(q):
+        target = int(input())
+        max_val = find_left(arr, target)
+        min_val = find_right(arr, target)
+        print(f"{max_val} {min_val}")
+
+main()
+
