21ef28f07e5dca6ed5ab840c338e9d3387dbd4d8
Mengting Niu <nmt@mail.ustc.edu.cn>
2025-08-28 14:24:15 +0800

    1

--- 二分/2_二分找下标问题.py
+++ 二分/2_二分找下标问题.py
@@ -1,3 +1,4 @@
+# 方法1 ：用二分法定位一个目标值，然后向左右扩展找边界
 def find_idx(arr, target):
     result = [-1,-1]
     n = len(arr)
@@ -30,3 +31,46 @@ def main():
 
 main()
     
+# 方法2：二分法变形
+# 对于左边界：
+# 即使nums[mid] == target, right仍然左移 
+# def find_first(A, x):
+#     left, right = 0,len(A)-1
+#     ans = -1
+#     while left <= right:
+#         mid = (left + right) //2
+#         if A[mid] == x:
+#             ans = mid
+#             right = mid-1
+#         elif A[mid] > x:
+#             right = mid-1
+#         else:
+#             left = mid+1
+#     return ans
+
+# def find_last(A, x):
+#     left, right = 0,len(A)-1
+#     ans = -1
+#     while left <= right:
+#         mid = (left + right) //2
+#         if A[mid] == x:
+#             ans = mid
+#             left = mid+1
+#         elif A[mid] > x:
+#             right = mid-1
+#         else:
+#             left = mid+1
+#     return ans
+
+# def main():
+#     n, q = map(int, input().split())
+#     A = list(map(int, input().split()))
+#     for _ in range(q):
+#         x = int(input())
+#         first = find_first(A, x)
+#         last = find_last(A, x)
+#         if first == -1:
+#             print("-1 -1")
+#         else:
+#             print(f"{first + 1} {last + 1}")  # 转为 1-based 下标
+# main()
